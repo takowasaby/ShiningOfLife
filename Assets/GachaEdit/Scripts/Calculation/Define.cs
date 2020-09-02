74ee5d81a17ac0cd13aa9b99a08ce8d9cb40ec11
祐介 富岡 <tomioka2012@gmail.com>
2020-09-02 16:02:02 +0900

    計算に必要なデータ型を定義

---
+++ Assets/GachaEdit/Scripts/Calculation/Define.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GachaRarity
+{
+    N,
+    R,
+    SR,
+    SSR,
+    UR,
+    LR,
+}
+
+public enum UserSegment
+{
+    MuKakin,
+    BiKakin,
+    TyuKakin,
+    JuKakin,
+    Sekiyuo,
+}
+
+public struct GachaRatios
+{
+    public Dictionary<GachaRarity, float> ratios;
+}
+
+public struct GachaCeiling
+{
+    public int value;
+}
+
+public struct GachaParameter
+{
+    public GachaRatios ratios;
+    public GachaCeiling ceiling;
+}
+
+public struct StoneDistribution
+{
+    public int count;
+}
+
+public struct PromotionCost
+{
+    public long cost;
+}
+
+public struct UserCount
+{
+    public Dictionary<UserSegment, long> ratios;
+}
+
+public struct UserSatisfaction
+{
+    public float value;
+}
+
+public struct MoneyBalance
+{
+    public long income;
+    public long expenditure;
+}
+
+public struct PlayerInput
+{
+    public GachaParameter gachaParam;
+    public StoneDistribution stoneDistrib;
+    public PromotionCost promoConst;
+}
+
+public struct InnerInput
+{
+    public UserCount userCount;
+    public UserSatisfaction satisfaction;
+}
+
+public struct GraphOutput
+{
+    public UserCount userCount;
+    public UserSatisfaction satisfaction;
+    public MoneyBalance balance;
+}
