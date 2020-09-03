9bd20471bc93dbbd51ba4a17dff02b07b7468e4c
祐介 富岡 <tomioka2012@gmail.com>
2020-09-03 22:13:36 +0900

    ガチャ確率のProgressを追加

---
+++ Assets/GachaEdit/Scripts/Progress/PremiumGachaRates.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Custom/PremiumGachaRates")]
+public class PremiumGachaRates : ScriptableObject
+{
+    public float nRate = 100f;
+    public float rRate = 0f;
+    public float srRate = 0f;
+    public float ssrRate = 0f;
+    public float urRate = 0f;
+    public float lrRate = 0f;
+}
