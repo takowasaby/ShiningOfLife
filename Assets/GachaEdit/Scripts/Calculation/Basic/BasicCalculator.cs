01d4324a58ceafcf326f98ed2c1affff54350605
祐介 富岡 <tomioka2012@gmail.com>
2020-09-03 21:57:17 +0900

    計算式を実装

---
+++ Assets/GachaEdit/Scripts/Calculation/Basic/BasicCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class BasicCalculator : MonoBehaviour
+{
+    public CalculationHalfway Calc(CalculationHalfway halfway)
+    {
+        // ユーザー数からIncomeを計算（Ceilingも反映）
+        // BounusRateを清算
+
+        // 順番はGachaCalculator、OtherCalculator、UserCalculator、BasicCalculator
+
+        var userCounts = halfway.innerInput.userCount.counts;
+        var ceilingCount = halfway.playerInput.gachaParams
+            .Where(param => param.Value.isCeiling)
+            .Count();
+        
+        var income =
+            userCounts[UserSegment.BiKakin] * 1000L +
+            (long)(userCounts[UserSegment.TyuKakin] * 10000L * (1 + ceilingCount * 0.1)) +
+            (long)(userCounts[UserSegment.JuKakin] * 50000L * (1 - ceilingCount * 0.1)) +
+            userCounts[UserSegment.Sekiyuo] * 1000000L;
+
+        halfway.graphOutput.balance.income = (long)(income * halfway.imValues.bonusRate);
+
+        return halfway;
+    }
+}
+
+
---
+++ Assets/GachaEdit/Scripts/Calculation/Gacha/GachaCalculator.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GachaCalculator : MonoBehaviour
+{
+    public CalculationHalfway Calc(CalculationHalfway halfway)
+    {
+        // それぞれのガチャの効果を算出
+        // 例）ノーマルガチャなら好感度を変更
+
+        // ついでにガチャ確率から個別にKindnessを割り出す
+        // 最高レアが10%（最も優しい）->6%（優しい）->1%（厳しい）->0.01%（最も厳しい）
+
+        // 個別のKindnessを平均して総合Kindnessを算出
+
+        var kindnesses = new float[6];
+
+        // ノーマルガチャ
+        if (halfway.playerInput.gachaParams.ContainsKey(GachaCategory.Normal))
+        {
+            var normalGachaparam = halfway.playerInput.gachaParams[GachaCategory.Normal];
+            kindnesses[0] = Mathf.Log10(Mathf.Clamp(normalGachaparam.rates.rates[GachaRarity.LR], 0.1f, 10f));
+            halfway.innerInput.satisfaction.value += kindnesses[0] * 3f;
+        }
+
+        // プレミアム
+        if (halfway.playerInput.gachaParams.ContainsKey(GachaCategory.Premium))
+        {
+            var premiumGachaparam = halfway.playerInput.gachaParams[GachaCategory.Premium];
+            kindnesses[1] = Mathf.Log10(Mathf.Clamp(premiumGachaparam.rates.rates[GachaRarity.LR], 0.1f, 10f));
+            halfway.imValues.bonusRate += kindnesses[1] * 0.2f;
+        }
+
+        // ボックス
+        if (halfway.playerInput.gachaParams.ContainsKey(GachaCategory.Box))
+        {
+            var boxGachaparam = halfway.playerInput.gachaParams[GachaCategory.Box];
+            kindnesses[2] = Mathf.Log10(Mathf.Clamp(boxGachaparam.rates.rates[GachaRarity.LR], 0.1f, 10f));
+            halfway.innerInput.satisfaction.value += kindnesses[0] * 10f;
+        }
+
+        // コンプ
+        if (halfway.playerInput.gachaParams.ContainsKey(GachaCategory.Complete))
+        {
+            var completeGachaparam = halfway.playerInput.gachaParams[GachaCategory.Complete];
+            kindnesses[3] = Mathf.Log10(Mathf.Clamp(completeGachaparam.rates.rates[GachaRarity.LR], 0.1f, 10f));
+            halfway.imValues.bonusRate += kindnesses[1];
+            halfway.innerInput.satisfaction.value -= 10f;
+        }
+
+        // 確定
+        if (halfway.playerInput.gachaParams.ContainsKey(GachaCategory.Confirm))
+        {
+            var confirmGachaparam = halfway.playerInput.gachaParams[GachaCategory.Confirm];
+            kindnesses[4] = 1f;
+            halfway.imValues.bonusRate -= 0.1f;
+            halfway.innerInput.satisfaction.value += 6f;
+        }
+
+        // 福袋
+        if (halfway.playerInput.gachaParams.ContainsKey(GachaCategory.LuckyBag))
+        {
+            var luckyBagGachaparam = halfway.playerInput.gachaParams[GachaCategory.LuckyBag];
+            kindnesses[5] = 1f;
+            halfway.imValues.bonusRate += 0.5f;
+            halfway.innerInput.satisfaction.value += 10f;
+        }
+
+        halfway.imValues.kindness = kindnesses.Average();
+
+        return halfway;
+    }
+}
