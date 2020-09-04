ffa983fe28ba042cff48411268043aba21d24b10
utaishi <taishi.uemura2@gmail.com>
2020-09-04 16:07:24 +0900

    Add　Message

---
+++ Assets/GachaAnimation/Scripts/saisyotyuui.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class saisyotyuui : MonoBehaviour
+{
+    [SerializeField] private MessageScrollBehavior behaviour;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        behaviour.RequestScroll("プレミアムガチャの様子をモニタリングしています");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
