783a2380c4e751c4f7979c0a18214e6bd4fa5a38
velengel <koutyan_dora_ouchi@yahoo.co.jp>
2020-09-04 16:20:26 +0900

    add 初期化

--- Assets/GameStart/Scripts/ToOpening.cs
+++ Assets/GameStart/Scripts/ToOpening.cs
@@ -7,9 +7,34 @@ public class ToOpening : MonoBehaviour
 {
     // Start is called before the first frame update
     private float zikanSwitch = 0.0f;
+
+    public CommandOpenness co;
+    public MoneyProgress mp;
+    public TermProgress tp;
+    public SatisfactionProgress sp;
+    public UserCountProgress ucp;
+    public PremiumGachaRates pgr;
+
     void Start()
     {
-        
+        ucp.MukakinUserCounts = new List<long>();
+        ucp.BikakinUserCounts = new List<long>();
+        ucp.TyukakinUserCounts = new List<long>();
+        ucp.JukakinUserCounts = new List<long>();
+        ucp.SekiyuoCounts = new List<long>();
+
+        tp.currentTerm = 0;
+
+        mp.moneys = new List<long>();
+
+        pgr.nRate = 100f;
+        pgr.rRate = 0f;
+        pgr.srRate = 0f;
+        pgr.ssrRate = 0f;
+        pgr.urRate = 0f;
+        pgr.lrRate = 0f;
+
+        sp.satisFactions = new List<float>();
     }
 
     // Update is called once per frame
