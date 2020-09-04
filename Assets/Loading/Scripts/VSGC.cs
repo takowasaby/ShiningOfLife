9f73d303d936c480fb3eed32f7c0c1642c3d2011
velengel <koutyan_dora_ouchi@yahoo.co.jp>
2020-09-04 17:40:51 +0900

    fps違いに対応、ダミーscriptableObject追加

---
+++ Assets/Loading/Scripts/VSGC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VSGC : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public CommandOpenness co;
+    public MoneyProgress mp;
+    public TermProgress tp;
+    public SatisfactionProgress sp;
+    public UserCountProgress ucp;
+    public PremiumGachaRates pgr;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
